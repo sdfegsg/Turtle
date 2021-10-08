cb404187ed6e3feb45095b6bfb885121a4678d27
Gt63s <mw.billionaire@gmail.com>
2021-10-08 09:47:19 +0200

    uebung weg

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 import turtle, time
 
-#Übung
-turtle.shape("turtle")
-turtle.left(90)
-turtle.backward(300)
-
-i = 300
-while i > 2:
-    turtle.forward(i)
-    turtle.left(20)
-    turtle.forward(i)
-    turtle.backward(i)
-    turtle.left(20)
-    turtle.forward(i)
-    turtle.backward(i)
-    turtle.right(40)
-
-    i = i / 2
-
-time.sleep(10)
-
 
 #1.
 # turtle.shape("turtle")
