0ea46f6c168074920956b26e526e49525feac3f0
Gt63s <mw.billionaire@gmail.com>
2021-10-08 09:46:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+import turtle, time
+
+#Übung
+turtle.shape("turtle")
+turtle.left(90)
+turtle.backward(300)
+
+i = 300
+while i > 2:
+    turtle.forward(i)
+    turtle.left(20)
+    turtle.forward(i)
+    turtle.backward(i)
+    turtle.left(20)
+    turtle.forward(i)
+    turtle.backward(i)
+    turtle.right(40)
+
+    i = i / 2
+
+time.sleep(10)
+
+
+#1.
+# turtle.shape("turtle")
+# time.sleep(5)
+
+#2.
+# turtle.shape("turtle")
+# turtle.bgcolor("black")
+# turtle.color("green")
+# turtle.forward(100)
+#
+# time.sleep(5)
+
+#3.
+# turtle.shape("turtle")
+# turtle.color("green")
+#
+#
+# def writeA():
+#     turtle.left(60)
+#     turtle.forward(100)
+#     turtle.right(120)
+#     turtle.forward(100)
+#     turtle.backward(50)
+#     turtle.right(120)
+#     turtle.penup()
+#     turtle.forward(50)
+#     turtle.right(180)
+#     turtle.pendown()
+#     turtle.forward(50)
+#     turtle.penup()
+#     turtle.right(90)
+#     turtle.forward(45)
+#
+# writeA()
+#
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.pendown()
+# turtle.forward(90)
+# turtle.right(90)
+#
+# for i in range(17):
+#     turtle.right(10)
+#     turtle.forward(8)
+#     i = i / 2
+#
+# turtle.left(170)
+# turtle.penup()
+# turtle.forward(50)
+# turtle.pendown()
+#
+# writeA()
+#
+# time.sleep(50)
+
+
+# 4.
+def zeichne_quadrat():
+    turtle.shape("turtle")
+    turtle.color("green")
+    for i in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+# 5.
+for i in range(32):
+    zeichne_quadrat()
+    turtle.right(11.25)
+
+
